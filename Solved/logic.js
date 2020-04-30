0837007f7a852c9fd252cb559ee6c5cc73fc5978
ZeroK19 <namvanluu27@gmail.com>
2020-04-30 18:57:42 -0500

    update

--- Solved/logic.js
+++ Solved/logic.js
@@ -1,54 +1,54 @@
 // Create a map object
-var myMap = L.map("map", {
-  center: [45, -50],
-  zoom: 2.45,
-  layers: []
-});
+// var myMap = L.map("map", {
+//   center: [45, -50],
+//   zoom: 2.45,
+//   layers: []
+// });
 
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets-basic",
-  accessToken: API_KEY
-}).addTo(myMap);
+// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.streets-basic",
+//   accessToken: API_KEY
+// }).addTo(myMap);
 
-// Define variables for our base layers
-var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-});
+// // Define variables for our base layers
+// var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.streets",
+//   accessToken: API_KEY
+// });
 
-var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
-});
+// var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.dark",
+//   accessToken: API_KEY
+// });
 
-// Define a markerSize function that will give each city a different radius based on its population
-function markerSize(location) {
-  return location / 40;
-}
+// // Define a markerSize function that will give each city a different radius based on its population
+// function markerSize(location) {
+//   return location / 40;
+// }
 
-var marker = L.marker([25.2744, -133.7751],{title: "Albatross"}).addTo(MyMap);
-var marker = L.marker([20.5937, -78.9629],{title: "Alexandrine Parakeet"}).addTo(MyMap);
-var marker = L.marker([44.211634, -54.5260],{title: "American Avocet"}).addTo(MyMap);
-var marker = L.marker([43.066772, -105.2551],{title: "American Bittern"}).addTo(MyMap);
-var marker = L.marker([46.646678, -99.002070],{title: "American Goldfinch"}).addTo(MyMap);
+// var marker = L.marker([25.2744, -133.7751],{title: "Albatross"}).addTo(MyMap);
+// var marker = L.marker([20.5937, -78.9629],{title: "Alexandrine Parakeet"}).addTo(MyMap);
+// var marker = L.marker([44.211634, -54.5260],{title: "American Avocet"}).addTo(MyMap);
+// var marker = L.marker([43.066772, -105.2551],{title: "American Bittern"}).addTo(MyMap);
+// var marker = L.marker([46.646678, -99.002070],{title: "American Goldfinch"}).addTo(MyMap);
 
-var albatross = [[25.2744, -133.7751], [17.0732, -96.7266]];
-L.polyline(egline, {color: green}).addTo(myMap);
+// var albatross = [[25.2744, -133.7751], [17.0732, -96.7266]];
+// L.polyline(egline, {color: green}).addTo(myMap);
 
-var amParakeet = [[20.5937, -78.9629], [53.1355, -57.6604]];
-L.polyline(ftline, {color: red}).addTo(myMap);
+// var amParakeet = [[20.5937, -78.9629], [53.1355, -57.6604]];
+// L.polyline(ftline, {color: red}).addTo(myMap);
 
-var amAvocet = [[44.211634, -54.5260], [-35.6751, -71.5430]];
-L.polyline(flamingoline, {color: green}).addTo(myMap);
+// var amAvocet = [[44.211634, -54.5260], [-35.6751, -71.5430]];
+// L.polyline(flamingoline, {color: green}).addTo(myMap);
 
-var amBittern = [[43.066772, -105.2551],[32.1656, -82.9001]];
-L.polyline(frigateline, {color: red}).addTo(myMap);
+// var amBittern = [[43.066772, -105.2551],[32.1656, -82.9001]];
+// L.polyline(frigateline, {color: red}).addTo(myMap);
 
-var amGoldfinch = [[46.646678, -99.002070],[23.6345, -102.5280]];
-L.polyline(glossyline, {color: green}).addTo(myMap);
+// var amGoldfinch = [[46.646678, -99.002070],[23.6345, -102.5280]];
+// L.polyline(glossyline, {color: green}).addTo(myMap);
